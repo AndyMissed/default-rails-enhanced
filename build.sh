b896c0dfe773ffb9b19e4f94126a0dc87258e85d
AndyMissed <47261430+AndyMissed@users.noreply.github.com>
2025-03-19 00:04:48 -0400

    Update build.sh to new naming scheme

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 # change the value of NMLC to reference your own nmlc installation
 NMLC="python3 $HOME/.local/bin/nmlc"
 ./clean.sh
-mkdir wannaroo-rails
-$NMLC -c -p DEFAULT --clear-orphaned wannaroo-rails.pnml
+mkdir default-rails-enhanced
+$NMLC -c -p DEFAULT --clear-orphaned default-rails-enhanced.pnml
 if [ $? -eq 0 ]
 then
-    cp wannaroo-rails.grf wannaroo-rails
-    cp README.md wannaroo-rails/readme.txt
-    cp LICENSE wannaroo-rails/license.txt
-    cp changelog.txt wannaroo-rails
-    tar cvf wannaroo-rails.tar wannaroo-rails
-    cp -i wannaroo-rails.tar $HOME/.openttd/newgrf
+    cp default-rails-enhanced.grf default-rails-enhanced
+    cp README.md default-rails-enhanced/readme.txt
+    cp LICENSE default-rails-enhanced/license.txt
+    cp changelog.txt default-rails-enhanced
+    tar cvf default-rails-enhanced.tar default-rails-enhanced
+    cp -i default-rails-enhanced.tar $HOME/.openttd/newgrf
     echo -e "\e[92m*** build successful ***"
 else
     echo -e "\e[91m*** nmlc could not build newgrf ***"
